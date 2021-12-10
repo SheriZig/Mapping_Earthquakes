39f600d48ec2d2475b45e58921c72da061ed236a
SheriZig <88912539+SheriZig@users.noreply.github.com>
2021-12-10 12:18:33 -0500

    Map with markers and circle

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,17 +2,37 @@
 console.log("working");
 
 //approximate center of tge united states
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the MAP object with a center and zoom level.
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
+//Add a MARKER to the map for Los Angeles, California
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
+//CIRCLE radius is based on meters
+let marker = L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: 'black',
+    fillColor: '#fef59f',
+    fillOpacity: 0.5,
+}).addTo(map);
+
+//CIRCLEMAKER is based on pixels. Therefore, the 'coverage area' changes based on zoom
+//var circleMaker = L.circleMaker([34.0522, -118.2437], {
+   // color: 'black',
+   // fillColor: '#fef59f',
+   // fillOpacity: 0.5,
+  // radius: 300
+//}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
+
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+console.log("done");
