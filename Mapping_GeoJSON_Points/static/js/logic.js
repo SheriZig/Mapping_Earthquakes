e9c23ad80d962274dfd2c7599b0ec8cba367a70f
SheriZig <88912539+SheriZig@users.noreply.github.com>
2021-12-12 10:48:35 -0500

    add geoJSON points

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,46 +3,10 @@ console.log("working");
 
 //approximate center of tge united states
 // Create the MAP object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+//let map = L.map('mapid').setView([30, 30], 2);
 
-// Add GeoJSON data. FeatureCollection object with PROPERTIES and GEOMETRY for 
-//      San Francisco Airport
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
 
-//Adding GeoJSON data to the map (sanFranAirport(see above))
-// dot notation for the 'directory' in the json for feature.properties.city
-/*L.geoJSON(sanFranAirport, {
-    //turn each feature into a marker o the map
-    pointToLayer: function(feature, latlng) {
-        console.log(feature);
-        return L.marker(latlng)
-        .bindPopup("<h2>" + feature.properties.city + ", " + feature.properties.state + "</h2> <hr> <h3>" + feature.properties.name + "</h3>" );
-    }
-}).addTo(map);*/
 
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup();
-     }
-}).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
@@ -52,8 +16,57 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
+//Create a base layer that holds both maps
+//BASE LAYER this will create the radio buttons in index.html for the user to select
+//the map that they want to display (streets or dark) from the tilelayers above
+let baseMaps = {
+    Street: streets, 
+    Dark: dark
+}
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//Alternative method using curly braces notation
+//Create the map object with a center and zoom level
+let map = L.map("mapid",{
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our LAYERS CONTROL and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// note: putting the data after the tilelayer loads map before adding data
+//access majorAirports.json
+let airportData = "https://raw.githubusercontent.com/SheriZig/Mapping_Earthquakes/main/majorAirports.json";
+
+//get GeoJSON data
+d3.json(airportData).then(function(data){
+    console.log(data);
+    //Creating a GeoJSON layer with the retrieved data
+    L.geoJSON(data,{
+        onEachFeature: function (feature, layer) {
+            
+            layer.bindPopup(
+            '<h1>'+ feature.properties.city + '</h1>'               
+            + 'Country: ' + feature.properties.country + '<br>'
+            + 'Name: ' + feature.properties.name + '<br>'
+            + 'Altitude: ' + feature.properties.alt + '<br>'
+            + 'DST: ' + feature.properties.dst + '<br>'
+            + 'FAA: ' + feature.properties.faa + '<br>'
+            + 'ICAO: ' + feature.properties.icao + '<br>'
+            + 'TZ: ' + feature.properties.tz + '<br>'
+               
+            );
+             
+            }
+        }).addTo(map);
+});
+
 console.log("done");
